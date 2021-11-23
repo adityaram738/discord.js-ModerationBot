f632c117003781e9126f13346ff281d0bfbbe9a0
adityaram738 <94916952+adityaram738@users.noreply.github.com>
2021-11-23 20:51:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Prefix = `!`;
-exports.Token = `your bot token`;
+exports.Prefix = `π`;
+exports.Token = `OTA1Njc4NjQxODYwNTk5ODI4.YYNk9g.BJAolz3g5Px9wcv1PGKeih7Wv4E`;
 exports.Color = `RANDOM`;
